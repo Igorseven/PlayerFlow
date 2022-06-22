6c6e4cc10dba2b03d6c1eaf08ccfbe1e0e06f1f6
João Antônio Medeiros de Souza <joaoasouza982@gmail.com>
2022-06-21 21:14:12 -0300

    Reset Password

---
+++ PlayerFlowX/PlayerFlowX.Domain/Identity/ResetPassword.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayerFlowX.Domain.Identity
+{
+    public class ResetPassword
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
